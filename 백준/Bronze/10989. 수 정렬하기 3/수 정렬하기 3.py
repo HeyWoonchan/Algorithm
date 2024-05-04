c0f22eb8418660c49e6c0fbfd07c9d250004e8b7
HeyWoonchan <60343369+HeyWoonchan@users.noreply.github.com>
2024-05-04 15:21:49 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 7592 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -0,0 +1,17 @@
+import sys
+
+def main():
+    N = int(sys.stdin.readline())
+    arr = [0]*10001
+
+    for _ in range(N):
+        num=int(sys.stdin.readline())
+        arr[num]+=1
+
+    for i in range(10001):
+        if arr[i]>0:
+            for _ in range(arr[i]):
+                print(i)
+
+main()
+
