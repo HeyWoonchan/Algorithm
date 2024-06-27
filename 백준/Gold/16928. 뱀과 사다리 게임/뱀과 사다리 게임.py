e61f78a26bb2d5ceafc12fb4f9da0d0079cc10d9
HeyWoonchan <60343369+HeyWoonchan@users.noreply.github.com>
2024-06-27 14:21:15 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 64 ms, Memory: 34176 KB -BaekjoonHub

--- 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.py
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.py
@@ -1,26 +1,17 @@
 from collections import deque
 N,M = map(int,input().split())
 arr = [0]*101
-sadari_dict={}
 for _  in range(N):
     x,y = map(int,input().split())
-    sadari_dict[x]=y
     arr[x]=y
-snake_dict={}
 for _ in range(M):
     u,v = map(int,input().split())
-    snake_dict[u]=v
     arr[u]=v
-now = 1
-
-
 def bfs():
     visited= [0]*101
-    q = deque([(1,0,'1 ')])
-    # dice_min = 99999
+    q = deque([(1,0)])
     while q:
-        now_pos, dice_num, route = q.popleft() 
-        # print(now_pos, dice_num)
+        now_pos, dice_num = q.popleft() 
         if now_pos==100:
             print(dice_num)
         for i in range(1,7):
@@ -29,12 +20,7 @@ def bfs():
                 continue
             if arr[next_pos]!=0:
                 next_pos = arr[next_pos]
-                # print(next_pos)
             if visited[next_pos]==0:
-                
-                q.append((next_pos,dice_num+1, route+str(next_pos)+' '))
+                q.append((next_pos,dice_num+1))
                 visited[next_pos]=1
 bfs()
-
-
-#010 4941 2491
