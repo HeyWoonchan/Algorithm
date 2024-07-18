3d2be2f9e7688cd6c6a0840165130aa7ed3f2fa5
HeyWoonchan <60343369+HeyWoonchan@users.noreply.github.com>
2024-07-18 19:17:07 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
@@ -1,17 +1,12 @@
 input_str=input()
-
 nums_arr = input_str.split('-')
 
 final =[]
 for i in range(len(nums_arr)):
-    tmp = nums_arr[i].split('+')
-    for j in range(len(tmp)):
-        tmp[j]=int(tmp[j])
+    tmp = map(int,nums_arr[i].split('+'))
     final.append(sum(tmp))
 
 result = final[0]
 for i in range(1,len(final)):
     result-=final[i]
 print(result)
-
-
