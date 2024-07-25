ffb222519c6370cf88cdedc1634c0cde3e56a524
HeyWoonchan <60343369+HeyWoonchan@users.noreply.github.com>
2024-07-25 12:28:28 +0900

    [Gold IV] Title: 알파벳, Time: 5576 ms, Memory: 151380 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -4,7 +4,7 @@ marr = [list(input()) for _ in range(R)]
 
 
 d = [(-1,0),(1,0),(0,1),(0,-1)]
-visited= [[0]*C for _ in range(R)]
+# visited= [[0]*C for _ in range(R)]
 max_len = 0
 history = [0]*(26)
 def dfs(r,c, length_before):
@@ -12,16 +12,16 @@ def dfs(r,c, length_before):
     nowalpha = ord(marr[r][c])-ord('A')
     if history[nowalpha]==1:
         return
-    visited[r][c]=1
+    # visited[r][c]=1
     history[nowalpha]=1
     max_len = max(max_len, length_before+1)
     for dr, dc in d:
         nr, nc= r+dr, c+dc
         if not (0<=nr<R and 0<=nc<C):
             continue
-        if visited[nr][nc]==0 and marr[r][c]!= marr[nr][nc]:
+        if history[ord(marr[nr][nc])-ord('A')] ==0  and marr[r][c]!= marr[nr][nc]:
             dfs(nr,nc,length_before+1)
-    visited[r][c]=0
+    # visited[r][c]=0
     history[nowalpha]=0
 
 dfs(0,0,0)
