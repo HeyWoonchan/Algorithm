ea1658d93bbd034dbd2ca14d7686182e334e9157
HeyWoonchan <60343369+HeyWoonchan@users.noreply.github.com>
2024-07-25 13:37:47 +0900

    [Gold V] Title: 내려가기, Time: 296 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2096. 내려가기/내려가기.py
+++ 백준/Gold/2096. 내려가기/내려가기.py
@@ -8,19 +8,7 @@ minarr = [maxarr[0],maxarr[1],maxarr[2]]
 
 for i in range(N-1):
     t1,t2,t3 = map(int,input().split())
-
-    max1 = t1+max(maxarr[0],maxarr[1])
-    max2 = t2+max(maxarr[0],maxarr[1],maxarr[2])
-    max3 = t3+max(maxarr[1],maxarr[2])
-
-    maxarr = [max1, max2,max3]
-
-    min1 = t1+min(minarr[0],minarr[1])
-    min2 = t2+min(minarr[0],minarr[1],minarr[2])
-    min3 = t3+min(minarr[1],minarr[2])
-    minarr = [min1,min2,min3]
+    maxarr = [t1+max(maxarr[0],maxarr[1]), t2+max(maxarr),t3+max(maxarr[1],maxarr[2])]
+    minarr = [t1+min(minarr[0],minarr[1]),t2+min(minarr),t3+min(minarr[1],minarr[2])]
     
 print(max(maxarr), min(minarr))
-    
-
-
