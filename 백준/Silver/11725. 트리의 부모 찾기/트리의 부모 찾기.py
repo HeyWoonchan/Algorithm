f99a31dc623c97d2b82631624f64332c5e504ee5
HeyWoonchan <60343369+HeyWoonchan@users.noreply.github.com>
2024-07-29 15:35:01 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 364 ms, Memory: 52904 KB -BaekjoonHub

---
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
@@ -0,0 +1,27 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+N = int(input())
+adj_list = [[] for _ in range(N+1)]
+for _ in range(N-1):
+    a,b = map(int, input().split())
+    adj_list[a].append(b)
+    adj_list[b].append(a)
+
+parents = [0]*(N+1)
+
+q = deque([1])
+visited= [0]*(N+1)
+
+while q:
+    node = q.popleft()
+
+    for nextnode in adj_list[node]:
+        if visited[nextnode]==0:
+            parents[nextnode]=node
+            q.append(nextnode)
+            visited[nextnode]=1
+
+print(*parents[2:], sep='\n')
+
