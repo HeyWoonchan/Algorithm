6752db9a658ab6a725feae1d7b684fb2233113ab
HeyWoonchan <60343369+HeyWoonchan@users.noreply.github.com>
2024-08-03 14:04:44 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.py
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.py
@@ -1,61 +1,21 @@
-#17070
-
 N = int(input())
 marr = [list(map(int,input().split())) for _ in range(N)]
 
-cnt = 0
+dp = [[[0,0,0] for _ in range(N+1)] for _ in range(N+1)]
+
 # state 0:가로, 1:세로, 2:대각선
+dp[1][2][0]=1
+# print(dp)
+# print()
+for i in range(N+1):
+    for j in range(1,N+1):
+        if (i,j)==(1,2):
+            continue
+        if marr[i-1][j-1]==1:
+            continue
+        dp[i][j][0]=dp[i][j-1][0]+dp[i][j-1][2]
+        dp[i][j][1]=dp[i-1][j][1]+dp[i-1][j][2]
+        if marr[i-2][j-1]== 0 and marr[i-1][j-2]==0:
+            dp[i][j][2]=sum(dp[i-1][j-1])
 
-def checknext(r,c, state):
-    if not (0<=r<N and 0<=c<N):
-        return False
-    if state==0  and marr[r][c]==0:
-        return True
-    if state==1 and marr[r][c]==0:
-        return True
-    if state==2:
-        flag = True
-        for dr,dc in [(0,0),(0,-1),(-1,0)]:
-            nr,nc = r+dr,c+dc
-            if marr[nr][nc]==1:
-                flag=False
-                break
-        return flag
-
-def sol(r,c,state):
-    global cnt,N
-
-    if state == 0:
-        for dr, dc, nextState in [(0,1,0),(1,1,2)]:
-            nr,nc = r+dr, c+ dc
-            #다음상태 체크
-            if checknext(nr,nc,nextState):
-                if (nr,nc)==(N-1,N-1):
-                    cnt+=1
-                else:
-                    sol(nr,nc,nextState)
-    elif state==1:
-        for dr, dc, nextState in [(1,0,1),(1,1,2)]:
-            nr,nc = r+dr, c+ dc
-            #다음상태 체크
-            if checknext(nr,nc,nextState):
-                if (nr,nc)==(N-1,N-1):
-                    cnt+=1
-                else:
-                    sol(nr,nc,nextState)
-    else:
-        for dr, dc, nextState in [(0,1,0),(1,0,1),(1,1,2)]:
-            nr,nc = r+dr, c+ dc
-            #다음상태 체크
-            if checknext(nr,nc,nextState):
-                if (nr,nc)==(N-1,N-1):
-                    cnt+=1
-                else:
-                    sol(nr,nc,nextState)
-
-
-if marr[N-1][N-1]==1:
-    print(0)
-else:
-    sol(0,1,0)
-    print(cnt)
+print(sum(dp[N][N]))
