528b5ba8ce48911e34e31b80deee132a56224518
HeyWoonchan <60343369+HeyWoonchan@users.noreply.github.com>
2024-09-08 15:53:22 +0900

    [level 2] Title: 피로도, Time: 58.84 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -8,13 +8,11 @@ def solution(k, dungeons):
         # print(_k, cnt)
         max_ = max(max_,cnt)
 
-        
         for i in range(len(_dungeons)):
             if not visited[i] and _k>=_dungeons[i][0]:
                 visited[i]=1
                 sol(_k-_dungeons[i][1], _dungeons, cnt+1)
                 visited[i]=0
-    
     sol(k,dungeons,0)
     
     return max_
