7b46ccce1ee432ab4472b28a7d872286082c9aae
HeyWoonchan <60343369+HeyWoonchan@users.noreply.github.com>
2025-12-22 21:09:30 +0900

    [Platinum V] Title: 전깃줄 - 2, Time: 184 ms, Memory: 127784 KB -BaekjoonHub

--- 백준/Platinum/2568. 전깃줄 － 2/전깃줄 － 2.py
+++ 백준/Platinum/2568. 전깃줄 － 2/전깃줄 － 2.py
@@ -1,16 +1,6 @@
 """
 전깃줄_2의 Docstring
 
-겹치지 않는다의 조건은
-2 2
-6 4
-7 6
-9 7
-10 10
-
-왼쪽을 1부터 순서대로 본다면, 연결되는 우측의 번호가 오름차순이어야 한다
-전깃줄을 제거했을 때, 오름차순이 이루어지도록 하기 위해 없애야 하는 최소 개수를 구하라.+무엇을 없애야 하는지.
-
 1 2 3 4 5 6 7 8 9 10
 8 2 9 
 
@@ -37,8 +27,8 @@ for i in range(1,500001):
     if pre[i]>0:
         arr.append(pre[i])
 
-dp=[arr[0]]
 trace=[0]*500001
+ansArr = []
 
 def bisearch(num):
     l,r = 0, len(dp)-1
@@ -49,7 +39,8 @@ def bisearch(num):
         else:
             l=mid+1
     return l
-ansArr = []
+
+dp=[arr[0]]
 for i in range(1,N):
     if dp[-1]<arr[i]:
         trace[arr[i]] = dp[-1]
@@ -63,27 +54,14 @@ for i in range(1,N):
             else:
                 trace[arr[i]]=dp[j-1]
 
+# 역추적 시작
 cur = dp[-1]
-# preAnswer = []
-
 while cur!=0:
-    # preAnswer.append(cur)
     pre[connected[cur]]=-1
     cur = trace[cur]
 
+#답 출력
 print(N-len(dp))
-
 for i in range(1,500001):
     if pre[i]>0:
         print(i)
-
-
-# print(preAnswer)
-#     print(dp)
-# print(dp)
-# print(len(dp))
-# print(ans)
-
-
-
-
