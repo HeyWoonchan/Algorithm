d76652013564781ad79652ccd3ae649ffcb50627
HeyWoonchan <60343369+HeyWoonchan@users.noreply.github.com>
2025-12-30 15:37:06 +0900

    [Silver II] Title: 통계학, Time: 344 ms, Memory: 53796 KB -BaekjoonHub

--- 백준/Silver/2108. 통계학/통계학.py
+++ 백준/Silver/2108. 통계학/통계학.py
@@ -1,47 +1,23 @@
+import sys
+input = sys.stdin.readline
 N = int(input())
+arr = [int(input()) for _ in range(N)]
 
-arr_1=[]
-arr= [0]*(8003)
-for _ in range(N):
-    num = int(input())
-    arr[num+4000]+=1
-    arr_1.append(num)
-
-arr_1.sort()
-
-mid = arr_1[N//2]
-
-
-many_1= 0
-
-cnt_many = 0
-for i in range(8003):
-    if arr[i]>cnt_many:
-        cnt_many = arr[i]
-
-manyarr = []
-for i in range(8003):
-    if arr[i]==cnt_many:
-        manyarr.append(i-4000)
-
-if len(manyarr)>=2:
-    many_1 = manyarr[1]
+print(round(sum(arr)/N))
+arr.sort()
+print(arr[(N)//2])
+count = [0]*(8002)
+for a in arr:
+    count[a]+=1
+manyArr = []
+manyAns = max(count)
+for i in range(-4000,4001):
+    if manyAns==count[i]:
+        manyArr.append(i)
+manyArr.sort()
+if len(manyArr)>1:
+    print(manyArr[1])
 else:
-    many_1 = manyarr[0]
-
-
-avg = sum(arr_1)/N
-# if avg>0:
-#     if avg-int(avg)>=0.5:
-#         avg = int(avg)+1
-#     else:
-#         avg = int(avg)
-# else:
-#     if avg+int(avg)<=-0.5:
-#         avg = int(avg)-1
-#     else:
-#         avg = int(avg)
-print(round(avg))
-print(mid)
-print(many_1)
-print(arr_1[-1]-arr_1[0])
+    print(manyArr[0])
+# print(manyArr)
+print(arr[-1]-arr[0])        
