e59a3434f09a42739f60ffda271088d10492605a
HeyWoonchan <60343369+HeyWoonchan@users.noreply.github.com>
2026-01-05 14:02:52 +0900

    [Silver III] Title: 계단 오르기, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.py
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -1,20 +1,14 @@
-# 계단 오르기
-N = int(input())
-stair = [int(input()) for _ in range(N)]
+n = int(input())
+arr = [int(input()) for _ in range(n)]
 
-dp2= [0]*(N+1)
+dp = [[0,0] for _ in range(n)]
+#이전 계단을 밟고 올라온 경우[1]/안밟고 올라온 경우[0]
+dp[0]=[arr[0],arr[0]]
+if n>1:
+    dp[1][1]=arr[0]+arr[1]
+    dp[1][0]=arr[1]
 
-for i in range(1,N+1):
-    if i==1:
-        dp2[i]=stair[i-1]
-    elif i==2:
-        dp2[i]=stair[i-1]+dp2[i-1]
-
-    elif i==3:
-        dp2[i]=stair[i-1]+max(stair[i-2],stair[i-3])
-    else:
-        dp2[i] = stair[i-1] + max(stair[i-2]+dp2[i-3], dp2[i-2])
-
-
-
-print(dp2[N])
+for i in range(2,n):
+    dp[i][1]=arr[i]+dp[i-1][0]
+    dp[i][0]=arr[i]+max(dp[i-2])
+print(max(dp[n-1]))
