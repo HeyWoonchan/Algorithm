c2c20f0b7265afd8e16e8c495fe69e8fd616fdd0
HeyWoonchan <60343369+HeyWoonchan@users.noreply.github.com>
2026-01-05 14:24:18 +0900

    [Silver III] Title: 바이러스, Time: 60 ms, Memory: 34900 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,31 +1,23 @@
-# 바이러스
-
 from collections import deque
+n=int(input())
+e=int(input())
+graph=[[] for _ in range(n+1)]
+for _ in range(e):
+    u,v = map(int,input().split())
+    graph[u].append(v)
+    graph[v].append(u)
 
-num_computers = int(input())
-num_connections = int(input())
+visited=[0]*(n+1)
+q = deque([1])
+visited[1]=1
+ans=0
+while q:
+    u = q.popleft()
+    if u!=1:
+        ans+=1
+    for v in graph[u]:
+        if visited[v]==0:
+            q.append(v)
+            visited[v]=1
+print(ans)
 
-
-graph = [[0]*num_computers for _ in range(num_computers)]
-for i in range(num_connections):
-    a, b = map(int, input().split())
-    graph[a-1][b-1] = 1
-    graph[b-1][a-1] =1
-
-visited = [0]*num_computers
-
-def bfs(num):
-    global visited
-    q = deque([num])
-    visited[num] = 1
-    
-    while q:
-        u = q.popleft()
-
-        for v in range(num_computers):
-            if visited[v]!= 1 and graph[u][v]==1:
-                q.append(v)
-                visited[v]=1
-    
-bfs(0)
-print(sum(visited)-1)
