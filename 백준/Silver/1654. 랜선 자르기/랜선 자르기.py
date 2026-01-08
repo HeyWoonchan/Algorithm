04442523fbdcd3717683fe948cd82a06b1815e8e
HeyWoonchan <60343369+HeyWoonchan@users.noreply.github.com>
2026-01-08 11:41:03 +0900

    [Silver II] Title: 랜선 자르기, Time: 88 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
@@ -1,67 +1,17 @@
-# 랜선 자르기
+import sys
+input = sys.stdin.readline
+K,N=map(int,input().split())
+arr = [int(input()) for _ in range(K)]
 
-K, N= map(int, input().split())
-
-
-
-"""
-
-
-11개를 만들기 위한 랜선의 최대 길이
-
-
-
-
-"""
-
-arr=list(int(input()) for _ in range(K))
-
-# arr.sort()
-
-# initial = sum(arr) //N
-
-
-# # bruteforce
-# while 1:
-#     sum = 0
-#     for i in range(len(arr)):
-#         sum+= arr[i]//initial
-    
-#     if sum == N:
-#         break
-#     else:
-#         initial -=1
-    
-
-
-    
-
-# binary search
-
-first = 1
-last = 2147483647
-answer=0
-while first <=last:
-    mid = (first + last) // 2
-    
-    # print(first, last,mid)
-    sum = 0
-    flag = True
-    for i in range(len(arr)):
-        sum+= arr[i]//mid
-    if sum>=N:
-        # print("N개보다 많이 나옴, 높여서 진행")
-        first = mid+1
-        answer = mid
+l,r = 1,2**31-1
+while l<=r:
+    mid = (l+r)//2
+    t = 0
+    for a in arr:
+        t+=a//mid
+    if t>=N:
+        l=mid+1
     else:
-        # print("N보다 적음 낮춰서 진행")
-        last = mid-1
+        r=mid-1
 
-# #check 
-
-
-#dp?
-
-      
-
-print(last)
+print(r)
