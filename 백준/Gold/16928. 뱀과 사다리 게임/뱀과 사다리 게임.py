5a0380435474d815e596f514a776e1c6b4b68913
HeyWoonchan <60343369+HeyWoonchan@users.noreply.github.com>
2026-01-15 13:26:15 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 60 ms, Memory: 35100 KB -BaekjoonHub

--- 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.py
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.py
@@ -1,26 +1,30 @@
 from collections import deque
-N,M = map(int,input().split())
-arr = [0]*101
-for _  in range(N):
-    x,y = map(int,input().split())
-    arr[x]=y
+N,M=map(int,input().split())
+ladders=[0]*101
+snakes=[0]*101
+for _ in range(N):
+    x,y=map(int,input().split())
+    ladders[x]=y
 for _ in range(M):
-    u,v = map(int,input().split())
-    arr[u]=v
-def bfs():
-    visited= [0]*101
-    q = deque([(1,0)])
-    while q:
-        now_pos, dice_num = q.popleft() 
-        if now_pos==100:
-            print(dice_num)
-        for i in range(1,7):
-            next_pos = now_pos+i
-            if next_pos>100:
-                continue
-            if arr[next_pos]!=0:
-                next_pos = arr[next_pos]
-            if visited[next_pos]==0:
-                q.append((next_pos,dice_num+1))
-                visited[next_pos]=1
-bfs()
+    u,v=map(int,input().split())
+    snakes[u]=v
+
+q=deque([(1,0)])
+visited=[0]*101
+visited[1]=1
+while q:
+    u,cnt=q.popleft()
+    if u==100:
+        print(cnt)
+        exit(0)
+    for i in range(1,7):
+        v=u+i
+        if not v<101:
+            continue
+        if ladders[v]!=0:
+            v=ladders[v]
+        if snakes[v]!=0:
+            v=snakes[v]
+        if visited[v]==0:
+            visited[v]=1
+            q.append((v,cnt+1))
