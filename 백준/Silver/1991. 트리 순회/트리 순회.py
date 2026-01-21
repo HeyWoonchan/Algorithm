92b07dbd7853836dbff3e1b6663a7882c44f9a35
HeyWoonchan <60343369+HeyWoonchan@users.noreply.github.com>
2026-01-21 15:10:58 +0900

    [Silver I] Title: 트리 순회, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1991. 트리 순회/트리 순회.py
+++ 백준/Silver/1991. 트리 순회/트리 순회.py
@@ -1,32 +1,43 @@
-def preorder(node):
-    if node =='.':
-        return
-    print(node,end="")
-    preorder(adj_list[node][0])
-    preorder(adj_list[node][1])
+def Atoi(a):
+    if a=='.':
+        return -1
+    return ord(a)-ord('A')
+def itoa(n):
+    return chr(n+ord('A'))
+
 
 def inorder(node):
-    if node=='.':
+    if node==-1:
         return
-    inorder(adj_list[node][0])
-    print(node,end="")
-    inorder(adj_list[node][1])
+    
+    inorder(tree[node][0])
+    print(itoa(node),end='')
+    inorder(tree[node][1])
+
+def preorder(node):
+    if node==-1:
+        return
+    print(itoa(node),end='')
+    preorder(tree[node][0])
+    preorder(tree[node][1])
 
 def postorder(node):
-    if node=='.':
+    if node==-1:
         return
-    postorder(adj_list[node][0])
-    postorder(adj_list[node][1])
-    print(node,end="")
+    postorder(tree[node][0])
+    postorder(tree[node][1])
+    print(itoa(node),end='')
 
-N = int(input())
-
-adj_list= dict()
+N=int(input())
+tree=[[-1]*2 for _ in range(N)]
 
 for _ in range(N):
-    node, left, right = input().split()
-    adj_list[node]=(left,right)
+    name, l,r = map(Atoi,input().split())
+    tree[name][0]=l
+    tree[name][1]=r
 
-preorder('A');print()
-inorder('A');print()
-postorder('A')
+preorder(0)
+print()
+inorder(0)
+print()
+postorder(0)
