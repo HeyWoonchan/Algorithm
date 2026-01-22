71f920ee704bca3f3c67525b82c27140ec766a65
HeyWoonchan <60343369+HeyWoonchan@users.noreply.github.com>
2026-01-22 16:07:03 +0900

    [Gold V] Title: 내려가기, Time: 216 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/2096. 내려가기/내려가기.py
+++ 백준/Gold/2096. 내려가기/내려가기.py
@@ -1,14 +1,24 @@
 import sys
 input = sys.stdin.readline
-
 N = int(input())
+# origintable = [list(map(int,input().split())) for _ in range(N)]
+minT=[]
+maxT=[]
+for i in range(N):
+    origintable=list(map(int,input().split()))
+    if i==0:
+        minT=[origintable[0],origintable[1],origintable[2]]
+        maxT=[origintable[0],origintable[1],origintable[2]]
+    if i>0:
+        tmpmin=[minT[0],minT[1],minT[2]]
+        tmpmin[0]=origintable[0]+min(minT[0],minT[1])
+        tmpmin[1]=origintable[1]+min(minT[0],minT[1],minT[2])
+        tmpmin[2]=origintable[2]+min(minT[1],minT[2])
+        minT=[tmpmin[0],tmpmin[1],tmpmin[2]]
 
-maxarr = list(map(int, input().split()))
-minarr = [maxarr[0],maxarr[1],maxarr[2]]
-
-for i in range(N-1):
-    t1,t2,t3 = map(int,input().split())
-    maxarr = [t1+max(maxarr[0],maxarr[1]), t2+max(maxarr),t3+max(maxarr[1],maxarr[2])]
-    minarr = [t1+min(minarr[0],minarr[1]),t2+min(minarr),t3+min(minarr[1],minarr[2])]
-    
-print(max(maxarr), min(minarr))
+        tmpmax=[maxT[0],maxT[1],maxT[2]]
+        tmpmax[0]=origintable[0]+max(maxT[0],maxT[1])
+        tmpmax[1]=origintable[1]+max(maxT[0],maxT[1],maxT[2])
+        tmpmax[2]=origintable[2]+max(maxT[1],maxT[2])
+        maxT=[tmpmax[0],tmpmax[1],tmpmax[2]]
+print(max(maxT),min(minT))
