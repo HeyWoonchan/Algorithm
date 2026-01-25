3f8b42f371e8ad1935eec4bd200161b489029182
HeyWoonchan <60343369+HeyWoonchan@users.noreply.github.com>
2026-01-25 20:58:26 +0900

    [Gold V] Title: 평범한 배낭, Time: 3868 ms, Memory: 228328 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -1,15 +1,26 @@
-N, K = map(int, input().split())
+N,K=map(int,input().split())
+things=[(0,0)]+[tuple(map(int,input().split())) for _ in range(N)]
 
-arr = [(0,0)]+[tuple(map(int, input().split())) for _ in range(N)]
-
-dp = [[0]*(K+1) for _ in range(N+1)]
-
-for i in range(N+1):
-    weight, value = arr[i]
+#넣거나 안넣거나
+#dp[i][j] i번째 물건까지 넣고, j무게까지 넣었을 때의 최대 가치
+dp=[[0]*(K+1) for _ in range(N+1)]
+# print(things)
+for i in range(1,N+1): 
+    w,v = things[i]
+    # print("물건i:",i,"w,v:",w,v)
     for j in range(K+1):
-        if weight>j:
+        # print("j:",j)
+        if w>j:
+            # print("w>j")
             dp[i][j]=dp[i-1][j]
         else:
-            dp[i][j]=max(dp[i-1][j], dp[i-1][j-weight]+value)
+            # print("w<=j")
+            dp[i][j]=max(dp[i-1][j],dp[i-1][j-w]+v)
+        # print(dp[i][j])
+    # print(*dp,sep='\n')
+    
 
-print(dp[N][K])
+
+
+# print(dp)
+print(dp[i][K])
