d610e041dd20d08f240a5a9ecd98b74e26ac3c5a
HeyWoonchan <60343369+HeyWoonchan@users.noreply.github.com>
2026-02-03 13:36:10 +0900

    [Silver IV] Title: 공금 횡령, Time: 208 ms, Memory: 46236 KB -BaekjoonHub

---
+++ 백준/Silver/34033. 공금 횡령/공금 횡령.py
@@ -0,0 +1,16 @@
+import sys;input=sys.stdin.readline
+N,M=map(int,input().split())
+normal = {}
+
+for _ in range(N):
+    name, price = input().split()
+    normal[name]=int(price)
+
+abnCnt=0
+for _ in range(M):
+    name, price=input().split()
+    if name not in normal:
+        continue
+    if normal[name]*105/100<int(price):
+        abnCnt+=1
+print(abnCnt)
