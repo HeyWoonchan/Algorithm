79bcfa6149fc3728073ac7bce5df09a545eed7a6
HeyWoonchan <60343369+HeyWoonchan@users.noreply.github.com>
2026-03-11 13:33:57 +0900

    [Bronze IV] Title: 파티가 끝나고 난 뒤, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2845. 파티가 끝나고 난 뒤/파티가 끝나고 난 뒤.py
+++ 백준/Bronze/2845. 파티가 끝나고 난 뒤/파티가 끝나고 난 뒤.py
@@ -1,3 +1,2 @@
-peoplePer1m2, partyArea = map(int,input().split())
-peopleInArticle = list(map(int,input().split()))
-print(*map(lambda x: x-peoplePer1m2*partyArea, peopleInArticle))
+a,b=map(int,input().split())
+print(*map(lambda x:x-a*b,map(int,input().split())))
