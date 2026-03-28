1118d1621da54a5a072ea964892ca428e2a08251
HeyWoonchan <60343369+HeyWoonchan@users.noreply.github.com>
2026-03-28 18:39:17 +0900

    [Gold I] Title: 외판원 순회, Time: 52 ms, Memory: 8576 KB -BaekjoonHub

---
+++ 백준/Gold/2098. 외판원 순회/외판원 순회.cc
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+const int INF = 1e9;
+int W[16][16];
+
+int main(){
+    int N;
+    cin>>N;
+    for (int i=0;i<N;i++){
+        for (int j=0;j<N;j++){
+            cin>>W[i][j];
+        }
+    }
+    // cout<<N<<'\n';
+    vector<vector<int>> dp((1<<N),vector<int>(N,INF));
+    //dp[mask|(1<<nextNode)][nextNode] = min(dp[mask][nextNode], dp[Mask|(1<<nextNode)][]+W[prev][nowNode])
+    for(int mask=0;mask<(1<<N)-1;mask++){
+        for(int now=0;now<N;now++){
+            if ((mask&(1<<now))==0){ //현재 방문 마스크 확인
+                continue;
+            }
+            for (int next=0;next<N;next++){
+                if (W[now][next]==0){
+                    continue;
+                }
+                if (mask&(1<<next)){
+                    continue;
+                }
+                if (mask==1){ //->x면 그냥 w[i][j]
+                    dp[mask|(1<<next)][next]=W[now][next];
+                    continue;
+                }
+
+                dp[mask|(1<<next)][next]=min(dp[mask|(1<<next)][next], dp[mask][now]+W[now][next]);
+
+            }
+        }    
+    }
+    int ans=INF;
+    for(int i=1;i<N;i++){
+        if (W[i][0]==0)continue;
+        ans = min(ans,dp[(1<<N)-1][i]+W[i][0]);
+    }
+    cout<<ans;
+}
