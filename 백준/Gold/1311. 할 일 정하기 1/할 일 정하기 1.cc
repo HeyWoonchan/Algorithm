2dae50ed11485f1bef768d8729d27b49166a721c
HeyWoonchan <60343369+HeyWoonchan@users.noreply.github.com>
2026-03-30 19:18:28 +0900

    [Gold I] Title: 할 일 정하기 1, Time: 124 ms, Memory: 6124 KB -BaekjoonHub

--- 백준/Gold/1311. 할 일 정하기 1/할 일 정하기 1.cc
+++ 백준/Gold/1311. 할 일 정하기 1/할 일 정하기 1.cc
@@ -4,25 +4,25 @@
 using namespace std;
 const int INF = 1e9;
 int D[20][20];
-vector<vector<int>> dp(1<<20,vector<int>(20,INF));
+vector<int> dp(1<<20,INF);
 
 int dfs(int nowPerson, int selected, int n){
     
     if (selected==((1<<n)-1)){
         return 0;
     }
-    if (dp[selected][nowPerson]!=INF){
-        return dp[selected][nowPerson];
+    if (dp[selected]!=INF){
+        return dp[selected];
     }
-    dp[selected][nowPerson]=INF;
+    dp[selected]=INF;
     // cout<<"dfs"<<" "<<nowPerson<<'\n';
     
 
     for (int i=0;i<n;i++){
         if (selected&(1<<i))continue;
-        dp[selected][nowPerson] = min(dp[selected][nowPerson], D[nowPerson][i]+dfs(nowPerson+1,selected|(1<<i),n));
+        dp[selected] = min(dp[selected], D[nowPerson][i]+dfs(nowPerson+1,selected|(1<<i),n));
     }
-    return dp[selected][nowPerson];
+    return dp[selected];
 }
 
 
@@ -37,7 +37,6 @@ int main(){
     }
     
     //dp[mask][now]=지금까지 고른거에서 출발해서 다고른..것
-    dp[0][0]=dfs(0,0,N);
-    cout<<dp[0][0];
+    cout<<dfs(0,0,N);
     return 0;
 }
