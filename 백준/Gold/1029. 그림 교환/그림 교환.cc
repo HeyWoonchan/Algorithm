f25666572788277395d5df857cf3d263d5c31ebd
HeyWoonchan <60343369+HeyWoonchan@users.noreply.github.com>
2026-03-31 12:28:34 +0900

    [Gold I] Title: 그림 교환, Time: 28 ms, Memory: 21224 KB -BaekjoonHub

---
+++ 백준/Gold/1029. 그림 교환/그림 교환.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+int dp[1<<15][15][10];
+int C[15][15];
+int ans=0;
+
+void dfs(int depth, int mask, int nowArtist, int lastCost, int maxPeople){
+    // cout<<"dfs"<<depth<<'\n';
+    ans = max(ans,depth);
+
+    int &res = dp[mask][nowArtist][lastCost];
+    if (res!=-1){
+        return;
+    }
+
+
+    for(int nextArtist=0;nextArtist<maxPeople;nextArtist++){
+        if ((mask&(1<<nextArtist))==0 && (lastCost<=C[nowArtist][nextArtist])){
+            
+            dfs(depth+1,mask|(1<<nextArtist),nextArtist,C[nowArtist][nextArtist],maxPeople);
+            dp[mask|(1<<nextArtist)][nextArtist][C[nowArtist][nextArtist]]=1;
+        }  
+    }
+
+}
+
+int main(){
+    memset(dp,-1, sizeof(dp));
+
+    int N;
+    cin>>N;
+    string s;
+    for(int i=0;i<N;i++){
+        cin>>s;
+        for(int j=0;j<N;j++){
+            C[i][j]=(int)(s[j]-'0');
+        }
+    }
+    // cout<<C[1][0];
+    dfs(1,1,0,0,N);
+    cout<<ans;
+}
