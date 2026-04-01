bf5c3a02dbf895afa5871cb7ae28db7001c09b26
HeyWoonchan <60343369+HeyWoonchan@users.noreply.github.com>
2026-04-01 20:48:44 +0900

    [Silver III] Title: 퇴사, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/14501. 퇴사/퇴사.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int N;
+int T[15];
+int P[15];
+
+int dfs(int nowDay){
+    if (nowDay>=N) return 0;
+    // cout<<nowDay<<" nowday\n";
+    int ret=dfs(nowDay+1); //안고르는경우
+    //오늘 것을 고르는 경우(N일 안에 끝나야함) 
+    if (nowDay+T[nowDay]<=N){
+        ret=max(ret,dfs(nowDay+T[nowDay])+P[nowDay]);
+    }
+    
+    //다음날로 넘어가는경우
+    
+    return ret;
+}   
+
+int main(){
+    cin>>N;
+    
+    for (int i=0;i<N;i++){
+        cin>>T[i]>>P[i];
+    }   
+
+    cout<<dfs(0);
+
+}
