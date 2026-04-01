33d6cfa65af72bc362341b2dabb9e4494fc1482f
HeyWoonchan <60343369+HeyWoonchan@users.noreply.github.com>
2026-04-01 12:10:51 +0900

    [Platinum V] Title: 발전소, Time: 24 ms, Memory: 2280 KB -BaekjoonHub

--- 백준/Platinum/1102. 발전소/발전소.cc
+++ 백준/Platinum/1102. 발전소/발전소.cc
@@ -13,7 +13,7 @@ const int INF = 1e9;
 //현재 켜진 것 들 중 i를 켜는 최솟값,다음 켤 것(i)
 //이미 다 켜서 켤 것이 없다면 0?
 int dfs(int nowOnMask){
-    if (__builtin_popcount(nowOnMask)==P)return 0;
+    if (__builtin_popcount(nowOnMask)>=P)return 0;
     
     // cout<<bitset<5>(nowOnMask)<<'\n';
     int &ret=dp[nowOnMask];
@@ -22,20 +22,10 @@ int dfs(int nowOnMask){
     ret = INF;
     for(int i=0;i<N;i++){ //i: 앞으로 켤 것
         if (nowOnMask&(1<<i))continue; //이미 켠 것은 또 켜지 말 것
-
-        int minFori=INF;
         for (int j=0;j<N;j++){ //j 이미 켜진 것들 중에서 최소비용 볼 것.
             if ((nowOnMask&(1<<j))==0) continue; //켜지지 않은 것은 제외
-            // if (j==i)continue;
-            minFori=min(minFori,C[j][i]);
+            ret=min(ret, C[j][i]+dfs(nowOnMask|(1<<i))); //앞으로 켤 것 중 최솟값+지금 켠 것 포함 앞으로 켜질 것들 중 최솟값
         }
-        // cout<<"앞으로 켤 것:"<<i<<'\n';
-        // cout<<"minfori:"<<minFori<<'\n';
-        if (minFori==INF){
-            return -1;
-        }
-        
-        ret=min(ret, minFori+dfs(nowOnMask|(1<<i))); //앞으로 켤 것 중 최솟값+지금 켠 것 포함 앞으로 켜질 것들 중 최솟값
     }
     return ret;
 }
@@ -61,21 +51,8 @@ int main(){
         cout<<0;
         return 0;
     }
-    int ans = INF;
-    for(int i=0;i<N;i++){
-        if (initMask&(1<<i))continue;
-        int minFori=INF;
-        for(int j=0;j<N;j++){
-            // if (i==j)continue;
-            if ((initMask&(1<<j))==0)continue;
-            minFori=min(minFori,C[j][i]);
-        }
-        // if (minFori==INF)minFori=0;
-        // cout<<"앞으로 켤 것:"<<i<<'\n';
-        // cout<<"minfori:"<<minFori<<'\n';
-        ans = min(ans,minFori+dfs(initMask|(1<<i)));
-        // cout<<"ansTmp:"<<ans<<'\n';
-    }
+    int ans=dfs(initMask);
+        // cout<<"ansTmp:"<<ans<<'\n'
     if (ans==INF){
         cout<<-1;
         return 0;
