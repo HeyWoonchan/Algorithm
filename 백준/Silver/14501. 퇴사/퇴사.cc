f09532c35f2f967e2f04ea40aacd0a30b93f6a54
HeyWoonchan <60343369+HeyWoonchan@users.noreply.github.com>
2026-04-01 20:53:04 +0900

    [Silver III] Title: 퇴사, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.cc
+++ 백준/Silver/14501. 퇴사/퇴사.cc
@@ -1,15 +1,21 @@
 #include <iostream>
-#include <vector>
+#include <cstring>
 #include <algorithm>
+
 using namespace std;
 int N;
 int T[15];
 int P[15];
+int dp[15];
 
 int dfs(int nowDay){
     if (nowDay>=N) return 0;
     // cout<<nowDay<<" nowday\n";
-    int ret=dfs(nowDay+1); //안고르는경우
+    if (dp[nowDay]!=-1){
+        return dp[nowDay];
+    }
+    int &ret=dp[nowDay];
+    ret = dfs(nowDay+1); //안고르는경우
     //오늘 것을 고르는 경우(N일 안에 끝나야함) 
     if (nowDay+T[nowDay]<=N){
         ret=max(ret,dfs(nowDay+T[nowDay])+P[nowDay]);
@@ -22,7 +28,7 @@ int dfs(int nowDay){
 
 int main(){
     cin>>N;
-    
+    memset(dp,-1,sizeof(dp));
     for (int i=0;i<N;i++){
         cin>>T[i]>>P[i];
     }   
