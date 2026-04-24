2435f6ac1482e95853f4a78f2decae7924955fd5
HeyWoonchan <60343369+HeyWoonchan@users.noreply.github.com>
2026-04-24 18:49:48 +0900

    [level 2] Title: H-Index, Time: 0.09 ms, Memory: 9.3 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,21 +1,10 @@
 def solution(citations):
-    ans = 0
-    left = 0
-    right = 100001
-    while left<=right:
-        h = (left+right)//2
-        over_h = 0
-        under_h = 0
-        for j in range(len(citations)):
-            if citations[j]>=h:
-                over_h+=1
-            elif citations[j]<=h:
-                under_h+=1
-        if over_h>=h and over_h+under_h==len(citations):
-            left=h+1
-        elif over_h<h:
-            right = h-1
-        else:
-            left = h+1
-                
-    return right
+    citations.sort()
+    
+    #h번 이상 인용된 논문이 h편 이상이고 나머지 논문이 h번 이하 인용되는 h의 최댓값
+    for i in range(0,len(citations)):
+        if citations[i]>=len(citations)-i:
+            return len(citations)-i
+            
+    
+    return 0
