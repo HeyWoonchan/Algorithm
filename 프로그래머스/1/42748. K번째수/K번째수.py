556285b73134f1dbbb5b5c59547c6fa8b301958f
HeyWoonchan <60343369+HeyWoonchan@users.noreply.github.com>
2026-04-26 11:32:32 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,2 +1,5 @@
 def solution(array, commands):
-    return [sorted(array[i-1:j])[k-1] for i,j,k in commands]
+    ans = []
+    for i,j,k in commands:
+        ans.append(sorted(array[i-1:j])[k-1])
+    return ans
