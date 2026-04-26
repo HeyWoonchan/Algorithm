358e25888e74c340f90a8ab60584773dc2ca6fae
HeyWoonchan <60343369+HeyWoonchan@users.noreply.github.com>
2026-04-26 11:49:28 +0900

    [level 3] Title: 네트워크, Time: 0.88 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,23 +1,23 @@
-def dfs(nowNode, visited, graph,n):
-    for i in range(n):
-        if i==nowNode:
-            continue
-        if visited[i]==1:
-            continue
-        if graph[nowNode][i]==1:
-            visited[i]=1
-            dfs(i,visited,graph,n)
+from collections import deque
 
+def bfs(nowNode, visited, graph):
+    q = deque([nowNode])
+    visited[nowNode]=1
+    
+    while q:
+        nowNode = q.popleft()
+        
+        for nextNode in range(len(graph[nowNode])):
+            if graph[nowNode][nextNode]==1 and visited[nextNode]==0:
+                visited[nextNode]=1
+                q.append(nextNode)
 
 def solution(n, computers):
     visited=[0]*n
     answer = 0
     for i in range(n):
         if visited[i]==0:
-            visited[i]=1
+            bfs(i,visited,computers)
             answer+=1
-            dfs(i,visited,computers,n)
-            
-    
     
     return answer
